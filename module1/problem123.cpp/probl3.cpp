8675ff618803f568f4ecfd7211ac69142dbe7670
Maksym Kozyr <maxkozyr2008@gmail.com>
2025-11-06 18:17:04 +0200

    add

--- module1/problem123.cpp/probl3.cpp
+++ module1/problem123.cpp/probl3.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
-#include <cmath>
-#include <vector>
+#include <string>
 using namespace std;
 typedef long long ll;
 typedef long double ld;
+
+void gt(string &s,ll a,ll b){
+    for(ll i=a;i<b;++i){
+        s[i]=s[i+1];
+    }
+}
+
 int main(){
-    vector<ll> a;
-    
+    string s;
+    cin>>s;
+    ll a=-1;
+    for(ll i=0;i<s.size();++i){
+        if((s[i]-'0')==0 || i==s.size()){
+            if(a==-1){
+                a=i;
+            }
+            else{
+                if(a!=-1){
+                    gt(s,a,i);
+                    a=-1;
+                }
+            }
+        }
+    }
+    if(a!=-1){
+        gt(s,a,s.size());
+        s=s+'0';
+    }
+    cout<<s;
     return 0;
 }
