27b8fbf0bbfe8afb16aef12bbcb4f3bc31534735
Maksym Kozyr <maxkozyr2008@gmail.com>
2025-11-07 11:04:06 +0200

    add

--- module1/problem123.cpp/probl2.cpp
+++ module1/problem123.cpp/probl2.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
-#include <cmath>
-using namespace std;
+#include <vector>
 typedef long long ll;
-typedef long double ld;
-int main(){
-    ll x1,x2,y1,y2;
-    cin>>x1>>y1>>x2>>y2;
-    ll dx=x2-x1;
-    ll dy=y2-y1;
-    double cos60=0.5;
-    double sin60=sqrt(3)/2.0;
-    double x3=x1+dx*cos60-dy*sin60;
-    double y3=y1+dx*sin60+dy*cos60;
-    cout<<x3<<" "<<y3;
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    ll n;
+    cin>>n;
+    vector<ll> a;
+    for(ll i=0;n>0;++i){
+        a.push_back(n%3);
+        n=n/3;
+    }
+    for(ll i=0;i<a.size();++i){
+        cout<<a[a.size()-i-1];
+    }
     return 0;
 }
