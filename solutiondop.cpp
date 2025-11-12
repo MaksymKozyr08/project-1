7cd1e83cf88be0c3f9e8b3b1044a1ebad1b94cba
Maksym Kozyr <maxkozyr2008@gmail.com>
2025-11-12 23:46:23 +0200

    add

--- solutiondop.cpp
+++ solutiondop.cpp
@@ -1,16 +1,17 @@
-#include <iostream>
+/*#include <iostream>
 #include <cmath>
 using namespace std;
 typedef long long ll;
 typedef long double ld;
 int main(){
-    ll a;
+    lв a;
     cin>>a;
     ld sum=0;
-    ll count=0;
-    for(ld i=1;sum<=a;++i){
+    for(ll i=1;sum<=a;++i){
         sum=sum+(1.0/i);
         if(sum>a)cout<<i;
     }
     return 0;
 }
+*/
+
